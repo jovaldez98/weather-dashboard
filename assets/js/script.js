58b45c56b5bb84fd02ae3f05700ee705da88e5c2
Joceliyn <jocevaldez98@gmail.com>
2023-03-31 02:01:12 -0700

    added more to html and js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var searchCity = document.getElementById('enter-city');
+var searchBtn = document.getElementById('search-button');
+var name = document.getElementById('city-name');
+var todayWeather= document.getElementById('today-weather');
+var currentImg = document.getElementById('current-img');
+var currentTemp = document.getElementById('temperature');
+var currentHum = document.getElementById('humidity');
+var currentWnd = document.getElementById('wind');
+var fiveDay = document.getElementById('fiveday-header');
+var ApiKey = "c177e89e479e96adeb080c247a54d560";
+var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + ApiKey;
+
+//fetch api url
+
+function getWeather(cityName) {
+    var 
+}
+
