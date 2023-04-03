f3e72461e0a785cc6d3134c9168f5bc1b030af98
Joceliyn <jocevaldez98@gmail.com>
2023-04-03 16:37:06 -0700

    added more code to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var searchCity = document.getElementById('enter-city');
 var searchBtn = document.getElementById('search-button');
+var clearBtn = document.getElementById('clear-history');
 var name = document.getElementById('city-name');
 var todayWeather= document.getElementById('today-weather');
 var currentImg = document.getElementById('current-img');
@@ -8,11 +9,45 @@ var currentHum = document.getElementById('humidity');
 var currentWnd = document.getElementById('wind');
 var fiveDay = document.getElementById('fiveday-header');
 var ApiKey = "c177e89e479e96adeb080c247a54d560";
-var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + ApiKey;
+var cityName = [];
 
 //fetch api url
 
+ searchBtn.addEventListener ('click', function() {
+    getWeather(searchCity.value);
+ })
+
+
 function getWeather(cityName) {
-    var 
+    var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + ApiKey + '&units=imperial';
+
+    fetch(queryUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        getForecastWeather(data.coord.lat, data.coord.lon);
+
+    })
 }
 
+function getForecastWeather(lat, lon) {
+    var queryUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + ApiKey + '&units=imperial';
+
+
+    fetch(queryUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+
+        //function call for creating weather cards
+    })
+}
+
+
+
+//weather icon url 
+//https://openweathermap.org/img/w/01d.png
