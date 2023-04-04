52b60cbce31ceb86a59cb5193eecdc79dcc204d6
Joceliyn <jocevaldez98@gmail.com>
2023-04-04 00:48:55 -0700

    added on readme and js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # weather-dashboard
+
+## Description
+Weather Dashboard was created using Bootstrap, JavaScript and API calls. Using an API key to gather information for a five-day weather cast in any city.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 var searchCity = document.getElementById('enter-city');
 var searchBtn = document.getElementById('search-button');
 var clearBtn = document.getElementById('clear-history');
-var name = document.getElementById('city-name');
+var nameEl = document.getElementById('city-name');
 var todayWeather= document.getElementById('today-weather');
 var currentImg = document.getElementById('current-img');
-var currentTemp = document.getElementById('temperature');
-var currentHum = document.getElementById('humidity');
-var currentWnd = document.getElementById('wind');
+var mainTempEl = document.getElementById('temperature');
+var mainHumEl = document.getElementById('humidity');
+var mainWndEl = document.getElementById('wind-speed');
 var fiveDay = document.getElementById('fiveday-header');
 var ApiKey = "c177e89e479e96adeb080c247a54d560";
 var cityName = [];
@@ -25,16 +25,33 @@ function getWeather(cityName) {
     .then(function (response) {
         return response.json();
     })
+
     .then(function (data) {
         console.log(data);
+
+         todayWeather.classList.remove("d-none");
+
+        // data to display current weather
+        var currentDate = new Date(data.dt * 1000);
+        var day = currentDate.getDate();
+        var month = currentDate.getMonth() + 1;
+        var year = currentDate.getFullYear();
+        nameEl.innerHTML = data.name + " (" + month + "/" + day + "/" + year + ") ";
+        var weatherImg = data.weather[0].icon;
+        currentImg.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherImg + "@2x.png");
+        currentImg.setAttribute("alt", data.weather[0].description);
+        mainTempEl.innerHTML = "temperature: " + (data.main.temp) + " &#176F";
+        mainHumEl.innerHTML = "humidity: " + data.main.humidity + "%";
+        mainWndEl.innerHTML = "wind-speed: " + data.wind.speed + " MPH";
+
         getForecastWeather(data.coord.lat, data.coord.lon);
+
 
     })
 }
 
 function getForecastWeather(lat, lon) {
     var queryUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + ApiKey + '&units=imperial';
-
 
     fetch(queryUrl)
     .then(function (response) {
@@ -49,5 +66,3 @@ function getForecastWeather(lat, lon) {
 
 
 
-//weather icon url 
-//https://openweathermap.org/img/w/01d.png
