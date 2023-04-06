06fffa141c99b7289bea777530e8cc578780c92a
Joceliyn <jocevaldez98@gmail.com>
2023-04-06 00:58:19 -0700

    finished, just adding to readme

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # weather-dashboard
 
 ## Description
-Weather Dashboard was created using Bootstrap, JavaScript and API calls. Using an API key to gather information for a five-day weather cast in any city.
+Weather Dashboard was created using Bootstrap, JavaScript and API calls. Using the 5 Day Weather Forecast website to get an API key to gather information for a current five-day weather in any city. Within the dashboard, there is information such as the current temperature, wind-speed, humidity and along with a weather icon. Local storage was used in JavaScript to save the city names that the user enters. A "clear search" button also exist to clear all the city names that the user had entered.
+
+
+## Screenshot of Weather Dashboard
+
+![Alt text](assets/images/weather-dashboard.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ var searchCity = document.getElementById('enter-city');
 var searchBtn = document.getElementById('search-button');
 var clearBtn = document.getElementById('clear-history');
 var nameEl = document.getElementById('city-name');
-var todayWeather= document.getElementById('today-weather');
+var todayWeather = document.getElementById('today-weather');
+var historyEl = document.getElementById('history');
 var currentImg = document.getElementById('current-img');
 var mainTempEl = document.getElementById('temperature');
 var mainHumEl = document.getElementById('humidity');
@@ -10,6 +11,7 @@ var mainWndEl = document.getElementById('wind-speed');
 var fiveDay = document.getElementById('fiveday-header');
 var ApiKey = "c177e89e479e96adeb080c247a54d560";
 var cityName = [];
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
 //fetch api url
 
@@ -47,9 +49,10 @@ function getWeather(cityName) {
         getForecastWeather(data.coord.lat, data.coord.lon);
 
 
-    })
+    });
 }
 
+// 5-day forecast
 function getForecastWeather(lat, lon) {
     var queryUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + ApiKey + '&units=imperial';
 
@@ -60,9 +63,74 @@ function getForecastWeather(lat, lon) {
     .then(function (data) {
         console.log(data);
 
-        //function call for creating weather cards
-    })
+        fiveDay.classList.remove("d-none");
+                        
+        //  display forecast for next 5 days
+
+        var forecastEls = document.querySelectorAll(".forecast");
+        for (i = 0; i < forecastEls.length; i++) {
+            forecastEls[i].innerHTML = "";
+            var forecastIndex = i * 8 + 4;
+            var forecastDate = new Date(data.list[forecastIndex].dt * 1000);
+            var forecastDay = forecastDate.getDate();
+            var forecastMonth = forecastDate.getMonth() + 1;
+            var forecastYear = forecastDate.getFullYear();
+            var forecastDateEl = document.createElement("p");
+            forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
+            forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+            forecastEls[i].append(forecastDateEl);
+
+            var weatherImgEl = document.createElement("img");
+            weatherImgEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[forecastIndex].weather[0].icon + "@2x.png");
+            weatherImgEl.setAttribute("alt", data.list[forecastIndex].weather[0].description);
+            forecastEls[i].append(weatherImgEl);
+            var forecastTempEl = document.createElement("p");
+            forecastTempEl.innerHTML = "Temp: " + (data.list[forecastIndex].main.temp) + " &#176F";
+            forecastEls[i].append(forecastTempEl);
+            var forecastHumEl = document.createElement("p");
+            forecastHumEl.innerHTML = "Humidity: " + data.list[forecastIndex].main.humidity + "%";
+            forecastEls[i].append(forecastHumEl);
+        }
+
+        getSearchHistory(searchBtn, clearBtn);
+    });
 }
 
 
+ // Local storage for search
 
+ searchBtn.addEventListener("click", function () {
+    var searchName = searchCity.value;
+    getWeather(searchName);
+    searchHistory.push(searchName);
+    localStorage.setItem("search", JSON.stringify(searchHistory));
+    getSearchHistory();
+});
+
+ // Clear History button
+
+ clearBtn.addEventListener("click", function () {
+    localStorage.clear();
+    searchHistory = [];
+    getSearchHistory();
+});
+
+function getSearchHistory() {
+    historyEl.innerHTML = "";
+    for (let i = 0; i < searchHistory.length; i++) {
+        var historyInput = document.createElement("input");
+        historyInput.setAttribute("type", "text");
+        historyInput.setAttribute("readonly", true);
+        historyInput.setAttribute("class", "form-control d-block bg-white");
+        historyInput.setAttribute("value", searchHistory[i]);
+        historyInput.addEventListener("click", function () {
+            getWeather(historyItem.value);
+        })
+        historyEl.append(historyInput);
+    }
+}
+
+getSearchHistory();
+if (searchHistory.length > 0) {
+    getWeather(searchHistory[searchHistory.length - 1]);
+}
